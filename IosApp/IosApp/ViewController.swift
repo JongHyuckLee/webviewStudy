eed45845e3ed0204c70bc1a120171cb090c60ee2
neo-korbit <neo@korbit.co.kr>
2021-09-10 16:28:00 +0900

    start ios

---
+++ IosApp/IosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosApp
+//
+//  Created by neo_local on 2021/09/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
