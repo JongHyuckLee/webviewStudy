a74060dfc7c294e9217ccf96be3f79d96ffb9ed2
neo-korbit <neo@korbit.co.kr>
2021-09-18 00:49:30 +0900

    start android webview

---
+++ androidWebView/app/src/main/java/com/example/androidwebview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.androidwebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("http://192.168.0.102:3000")
+
+    }
+}
