2bbe461f9352abb423f2c741bb9ba04ebea7477e
neo-korbit <neo@korbit.co.kr>
2021-09-30 02:53:17 +0900

    android - webview 스터디 코드 작성 완료

--- androidWebView/app/src/main/java/com/example/androidwebview/MainActivity.kt
+++ androidWebView/app/src/main/java/com/example/androidwebview/MainActivity.kt
@@ -1,18 +1,44 @@
 package com.example.androidwebview
 
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+class WebAppInterface (private val mContext: Context) {
+    @JavascriptInterface
+    fun showToast(toast: String) {
+        Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val webView: WebView = findViewById(R.id.webView)
         webView.settings.javaScriptEnabled = true
+        webView.webViewClient = CustomWebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.addJavascriptInterface(WebAppInterface(this), "Android")
+
+
         webView.loadUrl("http://192.168.0.102:3000")
 
+    }
+    private inner class CustomWebViewClient : WebViewClient() {
+        override fun onPageFinished(view: WebView, url: String) {
+            view.loadUrl("javascript:__SWIFT_CALL__.setBackgroundColor()")
+            view.loadUrl("javascript:__SWIFT_CALL__.setTextColor('red')")
+        }
     }
 }
